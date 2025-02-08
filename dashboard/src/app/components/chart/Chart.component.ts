1a08e634256c7d19c868a2e76ce79fe0bd1bcd11
LKomes <111873821+LKomes@users.noreply.github.com>
2025-02-08 23:52:56 +0000

    adding svgs and fixing imports

--- dashboard/src/app/components/chart/Chart.component.ts
+++ dashboard/src/app/components/chart/Chart.component.ts
@@ -17,45 +17,47 @@ export class ChartComponent implements OnInit {
   createChart() {
     const ctx = document.getElementById("myChart") as HTMLCanvasElement;
     new Chart(ctx, {
-      type: "line",
+      type: "bar",
       data: {
         labels: [
-          "Label",
-          "Label",
-          "Label",
-          "Label",
-          "Label",
-          "Label",
-          "Label",
-          "Label",
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
         ],
         datasets: [
           {
             label: "Label 1",
-            data: [4000, 3000, 2000, 2780, 1890, 2390, 3490, 3000],
-            borderColor: "#4887f6",
-            tension: 0.4,
+            data: [1000, 3000, 2000, 2780, 1890, 2390, 3490, 3000],
+            backgroundColor: "#4887f6",
+            
           },
           {
             label: "Label 2",
             data: [2500, 3500, 4000, 3200, 2800, 3900, 2900, 3500],
-            borderColor: "#59c3cf",
-            tension: 0.4,
+            backgroundColor: "#59c3cf",
           },
           {
             label: "Label 3",
             data: [1500, 2500, 3000, 2200, 1800, 2900, 1900, 2500],
-            borderColor: "#e2635e",
-            tension: 0.4,
+            backgroundColor: "#e2635e",
           },
         ],
       },
       options: {
         responsive: true,
         scales: {
+          x: {
+            stacked: true,
+          },
           y: {
+            stacked: true,
             beginAtZero: true,
-            max: 5000,
+            max: 10000,
             ticks: {
               stepSize: 1000,
             },
