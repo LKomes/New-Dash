8beafd2d4bd2d2c53c46462b1a97c101f204895f
LKomes <111873821+LKomes@users.noreply.github.com>
2025-02-08 23:23:25 +0000

    fising imports

--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataAnalysisComponent } from './components/dataAnalysis/DataAnalysis.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DataAnalysisComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
